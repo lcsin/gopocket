5c90165e783707c61572178326b619c9acf69b0b
lcsin <1847@qq.com>
2024-05-17 14:45:07 +0800

    new

---
+++ util/httpx/http_test.go
@@ -0,0 +1,63 @@
+package httpx
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestGinServer(t *testing.T) {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	type helloReq struct {
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+	}
+	r.POST("/hello", func(c *gin.Context) {
+		var req helloReq
+		if err := c.ShouldBind(&req); err != nil {
+			c.JSON(http.StatusBadRequest, "bad request")
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"name": req.Name,
+			"age":  req.Age,
+		})
+	})
+
+	Graceful(r, ":8080")
+}
+
+func TestGet(t *testing.T) {
+	resp, err := GET("http://localhost:8080/ping")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(resp))
+}
+
+func TestJSONPost(t *testing.T) {
+	type helloReq struct {
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+	}
+	req := helloReq{
+		Name: "张三",
+		Age:  18,
+	}
+	bytes, _ := json.Marshal(&req)
+	resp, err := POSTJson("http://localhost:8080/hello", bytes)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(resp))
+}
