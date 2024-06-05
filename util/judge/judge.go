a30a9c8af3d63378e260900a28bc76d433080b22
lcsin <1847@qq.com>
2024-06-05 10:07:22 +0800

    new

--- util/judge/judge.go
+++ util/judge/judge.go
@@ -19,3 +19,37 @@ func ContainChinese(text string) bool {
 	matched, _ := regexp.MatchString("\\p{Han}", text)
 	return matched
 }
+
+// 校验占位符是否配对
+func isBalancedChar(text string) bool {
+	stack := make([]rune, 0)
+
+	for _, char := range text {
+		switch char {
+		case '{', '[', '<', '「':
+			stack = append(stack, char)
+		case '}':
+			if len(stack) == 0 || stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case ']':
+			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case '>':
+			if len(stack) == 0 || stack[len(stack)-1] != '<' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case '」':
+			if len(stack) == 0 || stack[len(stack)-1] != '「' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
